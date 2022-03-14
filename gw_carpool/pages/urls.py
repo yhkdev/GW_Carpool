ae60d4ef27d199bf9f5e60a6d3dc7da70dba4e22
yhkdev <email@email.com>
2022-03-13 20:18:20 -0400

    setup templates

--- gw_carpool/pages/urls.py
+++ gw_carpool/pages/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('signup_driver', views.signup_driver, name='signup_driver'),
+    path('signup_rider', views.signup_rider, name='signup_rider'),
 ]
--- gw_carpool/pages/views.py
+++ gw_carpool/pages/views.py
@@ -1,10 +1,19 @@
-from http.client import HTTPResponse
-from urllib.request import HTTPRedirectHandler
-
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>index page</h1>')
+    return render(request, 'pages/index.html')
+
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+
+def signup_driver(request):
+    return render(request, 'pages/signup_driver.html')
+
+
+def signup_rider(request):
+    return render(request, 'pages/signup_rider.html')
